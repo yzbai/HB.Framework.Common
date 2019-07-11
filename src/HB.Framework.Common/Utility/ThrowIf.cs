3c62b63aa909caae4996a081acb744ae238485c0
yzbai <yzbai@brlite.com>
2019-07-11 18:51:04 +0800

    bug fixed

--- src/HB.Framework.Common/Utility/ThrowIf.cs
+++ src/HB.Framework.Common/Utility/ThrowIf.cs
@@ -4,9 +4,14 @@ using System.Text;
 
 namespace System
 {
+    [AttributeUsage(AttributeTargets.Parameter)]
+    public sealed class ValidatedNotNullAttribute : Attribute
+    {
+    }
+
     public static class ThrowIf
     {
-        public static T Null<T>(this T o, string paramName) where T : class
+        public static T Null<T>([ValidatedNotNull]T o, string paramName) where T : class
         {
             if (o == null)
                 throw new ArgumentNullException(paramName);
@@ -14,18 +19,51 @@ namespace System
             return o;
         }
 
-        public static string NullOrEmpty(this string o, string paramName)
+        public static string NullOrEmpty([ValidatedNotNull]string o, string paramName)
         {
-            if(string.IsNullOrEmpty(o))
+            if (string.IsNullOrEmpty(o))
             {
-                throw new ArgumentNullException(paramName);
+                throw new ArgumentException(paramName);
             }
 
             return o;
         }
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Globalization", "CA1303:Do not pass literals as localized parameters", Justification = "<Pending>")]
-        public static string NotEqual(this string a, string b, string paramName)
+        public static string NotEqual(string a, string b, string paramName)
+        {
+            if (a == null && b != null || a != null && !a.Equals(b, GlobalSettings.Comparison))
+            {
+                throw new ArgumentException("ThrowIf_NotEqual_Error_Message", paramName);
+            }
+
+            return a;
+        }
+    }
+    public static class ThrowIfExtensions
+    {
+
+
+        public static T ThrowIfNull<T>([ValidatedNotNull]this T o, string paramName) where T : class
+        {
+            if (o == null)
+                throw new ArgumentNullException(paramName);
+
+            return o;
+        }
+
+        public static string ThrowIfNullOrEmpty([ValidatedNotNull]this string o, string paramName)
+        {
+            if(string.IsNullOrEmpty(o))
+            {
+                throw new ArgumentException(paramName);
+            }
+
+            return o;
+        }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Globalization", "CA1303:Do not pass literals as localized parameters", Justification = "<Pending>")]
+        public static string ThrowIfNotEqual(this string a, string b, string paramName)
         {
             if (a == null && b!=null || a!=null && !a.Equals(b, GlobalSettings.Comparison))
             {
