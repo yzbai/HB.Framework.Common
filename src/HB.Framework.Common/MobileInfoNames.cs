8680ac98b8d61ceaa634299beeae291210da5ad7
yzbai <yzbai@brlite.com>
2019-07-19 11:45:13 +0800

    auto_commit

---
+++ src/HB.Framework.Common/MobileInfoNames.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HB.Framework.Common
+{
+    public static class MobileInfoNames
+    {
+        public const string DeviceId = "DeviceId";
+        public const string DeviceVersion = "DeviceVersion";
+        public const string DeviceType = "DeviceType";
+        public const string DeviceAddress = "DeviceAddress";
+
+        public const string AccessToken = "AccessToken";
+
+        public const string RefreshToken = "RefreshToken";
+    }
+}
