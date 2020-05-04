b334899393c29f9082501ee48da7160cf2353dc9
yzbai <yzbai@brlite.com>
2020-05-05 07:21:06 +0800

    添加FrameworkException，未来所有的Exception都要继承

---
+++ src/HB.Framework.Common/FrameworkException.cs
@@ -0,0 +1,28 @@
+﻿#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace System
+{
+    public class FrameworkException : Exception
+    {
+        public virtual FrameworkExceptionType ExceptionType { get; set; }
+
+        public FrameworkException(string? message) : base(message) { }
+
+        public FrameworkException(string? message, Exception? innerException) : base(message, innerException) { }
+
+        public FrameworkException() { }
+    }
+
+    public enum FrameworkExceptionType
+    {
+        Unkonwn = 0,
+        Database = 1,
+    }
+}
+
+#nullable restore
