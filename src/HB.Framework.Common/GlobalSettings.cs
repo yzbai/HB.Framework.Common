4f5607e4b1c19d05a994fe0fb25fe8df83f0b52b
yzbai <yzbai@brlite.com>
2020-05-30 16:10:55 +0800

    ss

--- src/HB.Framework.Common/GlobalSettings.cs
+++ src/HB.Framework.Common/GlobalSettings.cs
@@ -8,17 +8,17 @@ namespace System
 {
     public static class GlobalSettings
     {
-        [NotNull]public static CultureInfo Culture { get; set; } = CultureInfo.InvariantCulture;
+        [NotNull] public static CultureInfo Culture { get; set; } = CultureInfo.InvariantCulture;
 
-        [NotNull]public static StringComparison Comparison { get; set; } = StringComparison.InvariantCulture;
+        [NotNull] public static StringComparison Comparison { get; set; } = StringComparison.InvariantCulture;
 
-        [NotNull]public static StringComparison ComparisonIgnoreCase { get; set; } = StringComparison.InvariantCultureIgnoreCase;
+        [NotNull] public static StringComparison ComparisonIgnoreCase { get; set; } = StringComparison.InvariantCultureIgnoreCase;
 
-        [MaybeNull, DisallowNull]public static ILogger? Logger { get; set; }
+        [MaybeNull, DisallowNull] public static ILogger? Logger { get; set; }
 
         public static readonly string DateTimeFormat = "yyyy-MM-dd HH:mm:ss";
 
-        public static readonly string DebugTag = "==========================================HB_HB_HB===============================================";
+        public static readonly string LogTag = "=HB_HB_HB=";
 
     }
 }
